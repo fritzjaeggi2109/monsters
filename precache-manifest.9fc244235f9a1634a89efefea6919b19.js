54b7a487be7f4c60e82ec6ef7f57fd19c0787241
Federico Jaeggi <fritzjaeggi2109@gmail.com>
2020-06-09 18:04:41 -0300

    Updates

---
+++ precache-manifest.9fc244235f9a1634a89efefea6919b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed71955960ce229a3d8d31ea33c73880",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "aca2c9d99a8e52f4096b",
+    "url": "/monsters/static/css/main.18892e9a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca2c9d99a8e52f4096b",
+    "url": "/monsters/static/js/main.10815eea.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
